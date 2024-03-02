2f32b1f7a0a408afa8318bc93230bfcaf585d988
BenjaminWootten <bwootten@udel.edu>
2024-03-01 22:17:03 -0500

    grapple target implemented

--- grapple.gd
+++ grapple.gd
@@ -1,19 +1,37 @@
 extends Node2D
 
 @onready var ray = $RayCast2D
+@onready var target = $target
+
+const MAX_RAY_LENGTH = 400
 
 var moveable = true
+var target_position
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# Rotate and move target
 	if moveable:
-		look_at(get_global_mouse_position())
+		var mouse = get_global_mouse_position()
+		look_at(mouse)
+		print(ray.target_position)
+		if self.global_position.distance_to(mouse) < MAX_RAY_LENGTH:
+			ray.target_position.y = self.global_position.distance_to(mouse)/2
+		
+		if ray.is_colliding():
+			target.global_position = ray.get_collision_point()
+		elif self.global_position.distance_to(mouse) < MAX_RAY_LENGTH:
+			target.global_position = mouse
+	else:
+		target.global_position = target_position
 
 func get_ray_collision_point():
+	# Return nothing if not colliding
 	if not ray.is_colliding(): return
+	
+	# Return collision point, lock target
 	var collision = ray.get_collision_point()
-	if collision:
-		moveable = false
+	target_position = collision
+	moveable = false
 	return collision
 
 func release_grapple():
