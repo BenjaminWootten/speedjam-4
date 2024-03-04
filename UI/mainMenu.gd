b8bf5ab513137a81817453ad0552b17be0176c40
magnusculley <magnus@udel.edu>
2024-03-04 14:37:33 -0500

    level update

--- UI/mainMenu.gd
+++ UI/mainMenu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://levels/level_1.tscn")
+	get_tree().change_scene_to_file("res://level_base.tscn")
 
 
 func _on_quit_pressed():
